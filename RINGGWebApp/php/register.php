6c9fa04c08da186a5963b461a049ba99c9f62dad
Ricardo <ricardobegali87@gmail.com>
2024-11-07 10:56:08 -0300

    nav php e css register

--- RINGGWebApp/php/register.php
+++ RINGGWebApp/php/register.php
@@ -26,6 +26,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Cadastro</title>
+    <link rel="stylesheet" href="loginStyle.css">
+    <link rel="stylesheet" href="../../css/nav.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
     <h2>Cadastro</h2>
---
+++ nav.php
@@ -0,0 +1,23 @@
+<!-- navbar.php -->
+<nav>
+    <div class="icone-ringg"><a href="index.html"><img src="../img/img2.jpg"></a></div>
+    <div class="nav-mob">
+        <ul>
+            <a href="../quemsomos.html"><li><i class="fa-solid fa-question"></i></li></a>
+            <a href="../ajuda.html"><li><i class="fa-solid fa-circle-info"></i></li></a>
+            <a href="workstation.php"><li><i class="fa-solid fa-laptop"></i></li></a>
+            <a href="php/dashboard.php"><li><i class="fa-solid fa-user"></i></li></a>
+        </ul>
+    </div>
+    <div class="navbar">
+        <div class="op-nav">
+            <ul>
+                <a href="../quemsomos.html"><li>Quem Somos</li></a>
+                <a href="php/dashboard.php"><li>Dashboard</li></a>
+                <a href="../ajuda.html"><li>Ajuda</li></a>
+                <a href="workstation.php"><li>Espaço de Trabalho</li></a>
+                <a href="php/login.php"><li>ENTRAR</li></a>
+            </ul>
+        </div>
+    </div>
+</nav>
